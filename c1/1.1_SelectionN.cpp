c7c29391a91bc1407226a3aa5eab4c13007eb886
LinhNguyen <vulinh.bkhn@gmail.com>
2020-09-20 10:50:20 +0700

    add exercise c1.1

---
+++ c1/1.1_SelectionN.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+// selection sort
+void sort (int arr[], int N)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = i; j < N; ++j)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
+
+int find_Kth_Element (int arr[], int N, int k)
+{
+    if (k > N)
+        return -1;
+    else {
+        sort (arr, N);
+        return N - k;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int arr[] {3, 2, 1, 9, 8, 6, 5};
+    int i = find_Kth_Element(arr, sizeof(arr)/sizeof(int), 4);
+    printf ("k th element: %d\n", arr[i]);
+    return 0;   
+}
