98d19b7e9283e22f50a7122d1885690b26ac1ec6
LinhNguyen <vulinh.bkhn@gmail.com>
2020-09-21 22:23:03 +0700

    + [exercise 1_5] add

---
+++ c1/1_5_NumberOf1InTheBinary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int numbersOf1InBinary(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    } else {
+        return (n%2 == 0) ? numbersOf1InBinary(n/2) : numbersOf1InBinary(n/2) + 1; 
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int i = numbersOf1InBinary(63);
+    printf ("numbersOf1InBinary = %d\n", i);
+}
