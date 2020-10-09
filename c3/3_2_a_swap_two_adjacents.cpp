dadc3702c68d847c9f24da0945752cbd98b95fb6
vulinh.nguyen <vulinh.nguyen@lge.com>
2020-10-09 11:48:14 +0700

    [exercise 3_2_a] add

--- c3/3_2_a_swap_two_adjacents.cpp
+++ c3/3_2_a_swap_two_adjacents.cpp
@@ -11,11 +11,12 @@ struct Node {
 template <typename T>
 class List {
     public:
-        List () {
+        List () : size(0) {
             head = nullptr;
         }
         void push_back (Node<T> *& node)
         {
+            size++;
             if (!head) {
                 head = node;
             } else {
@@ -38,12 +39,45 @@ class List {
             std::cout << std::endl;
         }
         void swap_two_adjacent (int start_index) {
-
+            if (size - start_index < 2)
+            {
+                std::cout << "Could not swap!\n";
+                return;   
+            }
+            if (start_index == 0)
+            {
+                //N1 -> N2
+               Node<T> * tmp = head->next;
+               head->next = tmp->next;
+               tmp->next = head;  
+               head = tmp;
+            } else {
+                // tmp -> N1 -> N2 ->
+                Node<T> * tmp = head;
+                while (start_index > 1)
+                {
+                    start_index--;
+                    tmp = tmp->next;
+                }
+                Node<T> * node = tmp->next;
+                tmp->next = node->next;
+                node->next = node->next->next;
+                tmp->next->next = node;
+            }
         }
     private:
         Node<T> * head;
+        unsigned size;
 };
 
 int main (int argc, char* argv[]) {
     List<int> L;
+    Node<int> * node;
+    for (int i = 0; i < 10; ++i)
+    {
+        node = new Node<int> (i);
+        L.push_back(node);
+    }
+    L.swap_two_adjacent(4);
+    L.print();
 }
